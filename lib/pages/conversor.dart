54f9868579d84a131a5615a2d7f5e93661482fb6
Tiago Blosfeld Lopes <103440480+TiagoBlosfeld@users.noreply.github.com>
2022-04-15 14:13:43 -0300

    update 0.1

--- lib/pages/conversor.dart
+++ lib/pages/conversor.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class conversor extends StatefulWidget {
@@ -8,95 +7,150 @@ class conversor extends StatefulWidget {
 }
 
 class _conversorState extends State<conversor> {
-  TextEditingController graus = new TextEditingController();
+  TextEditingController graus = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    graus.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 60, left: 20, right: 20, bottom: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Insira a temperatura em °C :',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.all(15),
-            width: 180,
-            height: 40,
-            child: TextField(
-              controller: graus,
-              textAlign: TextAlign.left,
-              decoration: InputDecoration(
-                contentPadding:
-                    EdgeInsets.only(top: 8, bottom: 8, left: 0, right: 0),
-                hintText: 'Exemplo: 30°C',
-                hintStyle: TextStyle(
-                  color: Colors.black38,
-                ),
-                prefixIcon: Container(
-                  padding:
-                      EdgeInsets.only(top: 5, bottom: 5, left: 1, right: 1),
-                  child: Image.asset(
-                    'assets/temperatura.png',
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              keyboardType: TextInputType.number,
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 60, left: 20, right: 20, bottom: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
             ),
-          ),
-          Container(
-            height: 30,
-            width: 100,
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                primary: Colors.black,
-                backgroundColor: Colors.amberAccent,
-                side: BorderSide(width: 1, color: Colors.black),
-              ),
-              onPressed: () {
-                print('${graus.text}');
-              },
-              child: Container(
-                child: Text('Calcular'),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 75,
-          ),
-          Container(
-            padding: EdgeInsets.all(15),
-            height: 200,
-            width: double.infinity,
-            child: Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  children: [
-                    Text('K'),
-                  ],
+                Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Insira a temperatura em °C :',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.all(15),
+              width: 200,
+              height: 40,
+              child: TextField(
+                controller: graus,
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () => graus.clear(),
+                    icon: Icon(Icons.close),
+                  ),
+                  contentPadding: const EdgeInsets.only(
+                      top: 8, bottom: 8, left: 0, right: 0),
+                  hintText: 'Exemplo: 30°C',
+                  hintStyle: const TextStyle(
+                    color: Colors.black38,
+                  ),
+                  prefixIcon: Container(
+                    padding:
+                        EdgeInsets.only(top: 5, bottom: 5, left: 1, right: 1),
+                    child: Image.asset(
+                      'assets/temperatura.png',
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Container(
+              height: 30,
+              width: 100,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor: Colors.amberAccent,
+                  side: BorderSide(width: 1, color: Colors.black),
+                ),
+                onPressed: () {
+                  double grausint = double.parse(graus.text);
+                  double kelvin = grausint + 273;
+                  double reaumur = grausint * 0.8;
+                  double rankine = (grausint * 1.8) + 32 + 459.67;
+                  double fahren = (grausint * 1.8) + 32;
+
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      String reaumur1 = reaumur.toStringAsFixed(2);
+                      String rankine1 = rankine.toStringAsFixed(2);
+                      String fahren1 = fahren.toStringAsFixed(2);
+
+                      return AlertDialog(
+                        insetPadding: EdgeInsets.all(10),
+                        title: Text(
+                          'As conversões de ${graus.text}°C são:',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        content: Container(
+                          width: 300,
+                          height: 200,
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                  'Kelvin : ${graus.text}°C é igual a $kelvin K'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                  'Réaumur : ${graus.text}°C é igual a $reaumur1 Re'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                  'Rankine : ${graus.text}°C é igual a $rankine1 Ra'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                  'Fahrenheit : ${graus.text}°C é igual a $fahren1 F'),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  child: Text('Calcular'),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 75,
+            ),
+            Column(
+              children: [],
+            ),
+          ],
+        ),
       ),
     );
   }
